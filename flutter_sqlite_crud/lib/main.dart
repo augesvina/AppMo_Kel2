b3ab77daf9eeb9f0eea39797f15d9f85069f61e1
aaugesvina <89875584+augesvina@users.noreply.github.com>
2021-12-21 11:41:32 +0700

    Tubes AppMo
    Kelompok 2

---
+++ flutter_sqlite_crud/lib/main.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqlite_crud/db_helper.dart';
+import 'package:flutter_sqlite_crud/student_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StudentPage(),
+    );
+  }
+}
+
+class StudentPage extends StatefulWidget {
+  @override
+  _StudentPageState createState() => _StudentPageState();
+}
+
+class _StudentPageState extends State<StudentPage> {
+  final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
+  Future<List<Student>> students;
+  String _studentName;
+  bool isUpdate = false;
+  int studentIdForUpdate;
+  DBHelper dbHelper;
+  final _studentNameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DBHelper();
+    refreshStudentList();
+  }
+
+  refreshStudentList() {
+    setState(() {
+      students = dbHelper.getStudents();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mahasiswa TI'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Form(
+            autovalidateMode: AutovalidateMode.always,
+            key: _formStateKey,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Silahkan isi nama mahasiswa';
+                      }
+                      if (value.trim() == "")
+                        return "Isikan data dengan benar!";
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _studentName = value;
+                    },
+                    controller: _studentNameController,
+                    decoration: InputDecoration(
+                        focusedBorder: new UnderlineInputBorder(
+                            borderSide: new BorderSide(
+                                color: Colors.blue,
+                                width: 2,
+                                style: BorderStyle.solid)),
+                        // hintText: "Student Name",
+                        labelText: "Nama Mahasiswa",
+                        icon: Icon(
+                          Icons.business_center,
+                          color: Colors.blueAccent,
+                        ),
+                        fillColor: Colors.white,
+                        labelStyle: TextStyle(
+                          color: Colors.blue,
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                color: Colors.blue,
+                child: Text(
+                  (isUpdate ? 'UPDATE' : 'TAMBAH'),
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  if (isUpdate) {
+                    if (_formStateKey.currentState.validate()) {
+                      _formStateKey.currentState.save();
+                      dbHelper
+                          .update(Student(studentIdForUpdate, _studentName))
+                          .then((data) {
+                        setState(() {
+                          isUpdate = false;
+                        });
+                      });
+                    }
+                  } else {
+                    if (_formStateKey.currentState.validate()) {
+                      _formStateKey.currentState.save();
+                      dbHelper.add(Student(null, _studentName));
+                    }
+                  }
+                  _studentNameController.text = '';
+                  refreshStudentList();
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              RaisedButton(
+                color: Colors.red,
+                child: Text(
+                  (isUpdate ? 'CANCEL UPDATE' : 'CLEAR'),
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  _studentNameController.text = '';
+                  setState(() {
+                    isUpdate = false;
+                    studentIdForUpdate = null;
+                  });
+                },
+              ),
+            ],
+          ),
+          const Divider(
+            height: 5.0,
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: students,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return generateList(snapshot.data);
+                }
+                if (snapshot.data == null || snapshot.data.length == 0) {
+                  return Text('Tidak Ada Data Ditemukan');
+                }
+                return CircularProgressIndicator();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SingleChildScrollView generateList(List<Student> students) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: DataTable(
+          columns: [
+            DataColumn(
+              label: Text('NAMA'),
+            ),
+            DataColumn(
+              label: Text('HAPUS'),
+            )
+          ],
+          rows: students
+              .map(
+                (student) => DataRow(
+                  cells: [
+                    DataCell(
+                      Text(student.name),
+                      onTap: () {
+                        setState(() {
+                          isUpdate = true;
+                          studentIdForUpdate = student.id;
+                        });
+                        _studentNameController.text = student.name;
+                      },
+                    ),
+                    DataCell(
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          dbHelper.delete(student.id);
+                          refreshStudentList();
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
